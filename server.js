776518a05e59963cd33e6a16fc756721e5036fc3
James Murphy <jamesivdeveloper@gmail.com>
2022-03-06 20:38:19 -0600

    add html routes

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const { notes } = require("./Develop/db/db.json");
 const PORT = process.env.PORT || 3001;
+const path = require("path");
 
 const app = express();
+
+app.use(express.static("./Develop/public"));
 
 app.get("/api/notes", (req, res) => {
   res.json(notes);
   console.log(notes);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
 });
 
 app.listen(PORT, () => {
