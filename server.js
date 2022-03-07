d1803bf2ce45b5c0e7048eb01ecabaeabb1c6ad0
James Murphy <jamesivdeveloper@gmail.com>
2022-03-06 23:25:41 -0600

    add post data capability

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const { notes } = require("./Develop/db/db.json");
 const PORT = process.env.PORT || 3001;
 const path = require("path");
+const { createNewNote, findById } = require("./lib/notes.js");
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static("./Develop/public"));
 
@@ -20,6 +24,22 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
 });
 
+app.post("/api/notes", (req, res) => {
+  req.body.id = notes.length.toString();
+
+  const note = createNewNote(req.body, notes);
+  res.json(note);
+});
+/*
+app.delete("api/notes/:id", function (req, res) {
+  const result = findById(req.params.id, notes);
+  if (result) {
+    notes.remove(result);
+  } else {
+    res.send(404);
+  }
+});
+*/
 app.listen(PORT, () => {
   console.log(`API server listening on port ${PORT}!`);
 });
